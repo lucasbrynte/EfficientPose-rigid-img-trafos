Minor bug fix: Opencv function expected precisely a (2,) array for the center of rotation.

generators/common.py
@@ -473,10 +473,14 @@ class Generator(keras.utils.Sequence):
                 self.one_based_indexing_for_prewarp,
                 self.original_image_shape,
             )
+            cx = cx.squeeze()
+            cy = cy.squeeze()
+            assert cx.shape == ()
+            assert cy.shape == ()
 
         height, width, _ = img.shape
         #rotate and scale image
-        rot_2d_mat = cv2.getRotationMatrix2D((cx, cy), -inplane_angle, scale)
+        rot_2d_mat = cv2.getRotationMatrix2D(np.array([cx, cy]), -inplane_angle, scale)
         augmented_img = cv2.warpAffine(img, rot_2d_mat, (width, height))
         #append the affine transformation also to the mask to extract the augmented bbox afterwards
         augmented_mask = cv2.warpAffine(mask, rot_2d_mat, (width, height), flags = cv2.INTER_NEAREST) #use nearest neighbor interpolation to keep valid mask values